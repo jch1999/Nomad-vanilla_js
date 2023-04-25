1c43dc9b8f0b494256be4016ae0403595176ae7c
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-04-25 12:03:22 +0000

    app,js modified

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,1 +1,18 @@
-alert("hi")
+const a = 10;
+const b = 2;
+let myName = "chanho";
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("Hello " + myName);
+
+myName = "jch";
+
+console.log("Your new name is " + myName);
+var c = 6;
+var d = 3;
+console.log(c + d);
+c += 2;
+console.log(c * d);
+console.log(c / d);
