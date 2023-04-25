393a1481eb7736ed13ddd9a688d47b7abb02bca1
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-04-26 07:42:10 +0900

    app.js modified
    I studied about array, oject in js.
    it seems array is same as array in c++ and object is same as structure.
    it seems object's different is it can add elements, didn't have.
    js의 array와 object에 대해 공부했습니다.
    array는 c++의 array와 같고 object는 구조체와 같은 걸로 보입니다.
    object의 다른 점은 요소를 추가할 수 있다는 점인 것 것 같습니다.

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,3 +1,50 @@
-const amIFat=null;//null 비어있음을 의미하는 값이다. 비어진채로 채워진다. 비어있음을 의미하는 값으로 공간을 채운다.
-let smoeting;//undefined 공간은 존재하나 값이 없다.
-console.log(smoeting, amIFat);
+// -- array --
+// const days_of_week=["mon","tue","wed","thu","fri","sat"];
+
+// // Get Item from Aray
+// console.log(days_of_week);
+
+// //Add one more day to the array
+// days_of_week.push("sun");
+
+
+// // Get Item from Aray
+// console.log(days_of_week);
+
+// const toBuy=["potato","tomato","pizza"];
+// toBuy.push("kimbab");
+// console.log(toBuy[2]);
+// console.log(toBuy[5555555]);
+
+// --object--
+// const playerName="chanho"
+// const playerPoints=1212121;
+// const plauerHandsome=false;
+// const playerFat="little bit";
+
+// player.name
+// player.points
+// ...
+
+// player[0] == name
+// player[1] == points
+// const player=["chanho",12121212,false,"litle bit"];
+
+// object - 안에서 = 사용 안 함
+const player={
+    name:"chanho",
+    points: 120,
+    handsome: false,
+    fat:"little bit",
+};
+
+console.log(player);
+console.log(player.name);
+console.log(player["name"]);
+player.name;
+
+console.log(player);
+player.handsome=true;
+player.lastName="Jung";
+player.points=player.points+15;
+console.log(player);
