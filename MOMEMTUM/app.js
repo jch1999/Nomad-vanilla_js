14a8cf7c21b84bf6eb4cfab3983228a0daa1fc93
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-04-26 21:37:14 +0900

    js function study 2

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,11 +1,36 @@
-function sayHello() {
-  console.log("Hello my name is ");
-}
+// function sayHello(nameOfPerson,age) {
+//   console.log("Hello my name is "+nameOfPerson+" and I'm "+age+" yeasr old.");
+// }
 
-alert();
-console.log();
-sayHello("Chanho");
-sayHello("Nico");
-sayHello("Mary");
-sayHello("IB");
-sayHello("Garry");
+// alert();
+// console.log();
+// sayHello("Chanho",25);
+// sayHello("Nico",10);
+// sayHello("Mary",12);
+// sayHello("IB",12);
+// sayHello("Garry",24);
+
+// function plus (firstNumber,secondNumber)
+// {
+//     console.log(firstNumber+secondNumber);
+// }
+// // console.log(firstNumber);
+// function divide(a,b)
+// {
+//     console.log(a/b);
+// }
+
+// // plus();
+// plus(2,5);
+// divide(98,20);
+
+const player={
+    name: "chanho",
+    sayHello:function(otherPersonsName)
+    {
+        console.log("Hello "+otherPersonsName);
+    }
+};
+
+console.log(player.name);
+player.sayHello("Mary");
