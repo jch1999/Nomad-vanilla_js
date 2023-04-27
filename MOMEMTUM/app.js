634858615592fce7ececa69105107c677abba18b
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-04-27 07:54:12 +0000

    study  about 'return' in js

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,36 +1,38 @@
-// function sayHello(nameOfPerson,age) {
-//   console.log("Hello my name is "+nameOfPerson+" and I'm "+age+" yeasr old.");
-// }
-
-// alert();
-// console.log();
-// sayHello("Chanho",25);
-// sayHello("Nico",10);
-// sayHello("Mary",12);
-// sayHello("IB",12);
-// sayHello("Garry",24);
-
-// function plus (firstNumber,secondNumber)
-// {
-//     console.log(firstNumber+secondNumber);
-// }
-// // console.log(firstNumber);
-// function divide(a,b)
-// {
-//     console.log(a/b);
-// }
-
-// // plus();
-// plus(2,5);
-// divide(98,20);
-
-const player={
-    name: "chanho",
-    sayHello:function(otherPersonsName)
-    {
-        console.log("Hello "+otherPersonsName);
-    }
+const Calculator = {
+  Add: function(a, b) {
+    return (a + b);
+  },
+  Sub: function(a, b) {
+    return (a - b);
+  },
+  Div: function(a, b) {
+    return (a / b);
+  },
+  Mul: function(a, b) {
+    return (a * b);
+  },
+  Power: function(a, b) {
+    return (a ** b);
+  },
 };
 
-console.log(player.name);
-player.sayHello("Mary");
+// Calculator.Add(5, 5);
+// Calculator.Sub(5, 5);
+// Calculator.Mul(5, 5);
+// Calculator.Div(5, 5);
+// Calculator.Power(5, 5);
+// console.log(Calculator.Add(2, 3));
+
+// const age = 96;
+// function calculateKrAge(ageOfForeigner) {
+//   //return ageOfForeigner + 2
+//   return "hello";
+// }
+// const krAge = calculateKrAge(age)
+// console.log(krAge);
+
+const plusResult = Calculator.Add(2, 3);
+const minusResult = Calculator.Sub(plusResult, 10);
+const timesResult = Calculator.Mul(10, minusResult);
+const divideResult = Calculator.Div(timesResult, plusResult);
+const powerResult = Calculator.Power(divideResult, minusResult);
