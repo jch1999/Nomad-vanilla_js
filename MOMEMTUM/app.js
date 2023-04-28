841920bdcdb01ed060ef51901b050fca9816d657
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-04-28 06:04:53 +0000

    start learning about javaScript on the Browser, first about document

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,20 +1,6 @@
-const age=parseInt(prompt("How old are ypu?"));
-// console.log(isNaN(age));
+const title = document.getElementById("title");
 
-// if(condition){
-//     //conditino == true
-// } else {
-//   //condition == false
-// }
+title.innerText = "Got You!";
 
-if(isNaN(age)||age<0) {
-  console.log("Please write a real positive number");
-}else if(age<18) {
-  console.log("You are too young.")
-}else if(age>=18&&age<=50) {
-  console.log("You can drink.");
-}else if(age>50&&age<=80){
-  console.log("You should exercise.");
-}else if(age>=80){
-  console.log("You can do whateveryou want.");
-}
+console.log(title.id);
+console.log(title.className);
