ce289c92e69b1c23790e9d7faaf77b34b966f023
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-05-02 08:04:09 +0900

    event part two

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -4,12 +4,25 @@
 // console.dir(title);
 
 // title.style.color="blue";
-
 const title=document.querySelector("div.hello:first-child h1");
+
+console.log(title);
 
 function handleTitleClick()
 {
     //console.log("title was clicked!");
     title.style.color="blue";
 }
+
+function handleMouseEnter(){
+    title.innerText="Mouse is here!";
+}
+
+function handleMouseLeave(){
+    title.innerText="Mouse is gone!";
+}
+
 title.addEventListener("click",handleTitleClick);
+
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave",handleMouseLeave);
