4c4c0324263523ddb947b88cc017ecfd92c90518
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-05-01 23:57:40 +0900

    study about event in js
    addEventListener("eventName",functionName)

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,4 +1,15 @@
-const title = document.querySelectorAll(".hello h1");
+// const title = document.querySelector("div.hello:first-child h1 ");
 
 // title.innerText="hello";
-console.log(title);
+// console.dir(title);
+
+// title.style.color="blue";
+
+const title=document.querySelector("div.hello:first-child h1");
+
+function handleTitleClick()
+{
+    //console.log("title was clicked!");
+    title.style.color="blue";
+}
+title.addEventListener("click",handleTitleClick);
