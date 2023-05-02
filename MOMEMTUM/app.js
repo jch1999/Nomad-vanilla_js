220874d8f50c51932b5f39b657b43f8a4a0a5dc3
정찬호 <54787512+jch1999@users.noreply.github.com>
2023-05-02 23:54:38 +0900

    css in js 1

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,52 +1,18 @@
-// const title = document.querySelector("div.hello:first-child h1 ");
-
-// title.innerText="hello";
-// console.dir(title);
-
-// title.style.color="blue";
 const h1=document.querySelector("div.hello:first-child h1");
 
 console.log(h1);
 
 function handleTitleClick()
 {
-    //console.log("title was clicked!");
-    h1.style.color="blue";
+    const currentColor=h1.style.color;
+    let newColor;
+    console.log(h1.style.color);
+    if(currentColor==="tomato"){
+        newColor="blue";
+    }else{
+        newColor="tomato";
+    }
+    h1.style.color=newColor;
 }
 
-function handleMouseEnter(){
-    h1.innerText="Mouse is here!";
-}
-
-function handleMouseLeave(){
-    h1.innerText="Mouse is gone!";
-}
-
-function handleWindowResize(){
-document.body.style.backgroundColor="tomato";
-}
-
-function handleWindowCopy(){
-    alert("copier!");
-}
-
-function handleWindowOffline(){
-    alert("SOS no WIFI");
-}
-
-function handleWindowOnline(){
-    alert("All good");
-}
-
-//title.addEventListener("click",handleTitleClick);
-h1.onclick=handleTitleClick;
-
-//title.addEventListener("mouseenter",handleMouseEnter);
-h1.onmouseenter=handleMouseEnter;
-//title.addEventListener("mouseleave",handleMouseLeave);
-h1.onmouseleave=handleMouseLeave;
-
-window.addEventListener("resize",handleWindowResize);
-window.addEventListener("copy",handleWindowCopy);
-window.addEventListener("offline",handleWindowOffline);
-window.addEventListener("onine",handleWindowOnline);
+h1.addEventListener("click",handleTitleClick);
