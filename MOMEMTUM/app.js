46657bff1a6cf21633f4509415a8979366604184
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-05-03 08:11:23 +0000

    Form submission

--- MOMEMTUM/app.js
+++ MOMEMTUM/app.js
@@ -1,19 +1,25 @@
-const loginForm=document.getElementById("login-form");
+const loginForm = document.getElementById("login-form");
 //if I use id, upon code should be 
 //const loginForm=document.getElenetByID("login-from");
 //id should be started with #, but js already know we use id. so we don't need to
 //# in getElementById
-const loginInput=loginForm.querySelector("input");
-const loginButton=loginForm.querySelector("button");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 // const loginInput=document.querySelector("#login-input input");
 // const loginButton=document.querySelector("#login-input button");
 
 let login;
-function handleLoginButtonClick(){
-    login=loginInput.value;
-    console.log(loginInput.value);
-    console.log(login);
-    // console.log("clicked!");
+function handleLoginButtonClick() {
+  const username = loginInput.value;
+  // if (username == "") {
+  //   alert("Please write your name");
+  // } else if (username.length > 15) {
+  //   alert("Your name is too long.")
+  // } else {
+  //   console.log(username);
+  // }
+  console.log(username);
+  // console.log("clicked!");
 }
 
-loginButton.addEventListener("click",handleLoginButtonClick);
+loginButton.addEventListener("click", handleLoginButtonClick);
