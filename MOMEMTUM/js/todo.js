8d07af1e774bd304eda429b5c11464dc1af7a8ab
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-05-11 08:17:10 +0000

    loding todo2 & deleting todo 1,2

--- MOMEMTUM/js/todo.js
+++ MOMEMTUM/js/todo.js
@@ -2,50 +2,58 @@ const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 
-const TODOS_KEY="toDos";
+const TODOS_KEY = "toDos";
 
-const toDos=[];
+let toDos = [];
 
-function saveToDos(){
-  localStorage.setItem("toDos",JSON.stringify(toDos));
+function saveToDos() {
+  localStorage.setItem("toDos", JSON.stringify(toDos));
 }
 
-function deleteToDo(event){
-  const li=event.target.parentElement;
+function deleteToDo(event) {
+  const li = event.target.parentElement;
   li.remove();
 }
 
-function paintToDo(newToDo){
-  const li=document.createElement("li");
-  const span=document.createElement("span");
-  span.innerText=newToDo;
-  const button=document.createElement("button");
-  button.innerText="❌";
-  button.addEventListener("click",deleteToDo);
+function paintToDo(newToDoObj) {
+  const li = document.createElement("li");
+  li.id = newToDoObj.id;
+  const span = document.createElement("span");
+  span.innerText = newToDoObj.text;
+  const button = document.createElement("button");
+  button.innerText = "❌";
+  button.addEventListener("click", deleteToDo);
   li.appendChild(span);
   li.appendChild(button);
 
   toDoList.appendChild(li);
 }
+
 function handleToDoSubmit(event) {
   event.preventDefault();
   const newToDo = toDoInput.value;
   toDoInput.value = "";
-  toDos.push(newToDo);
-  paintToDo(newToDo);
+  const newToDoObj = {
+    text: newToDo,
+    id: Date.now(),
+  };
+  toDos.push(newToDoObj);
+  paintToDo(newToDoObj);
   saveToDos();
 }
 
 toDoForm.addEventListener("submit", handleToDoSubmit);
 
-function sayHello(item){
-  console.log("this is turn of a "+item);
+function sayHello(item) {
+  console.log("this is turn of a " + item);
 }
-const savedToDos=localStorage.getItem(TODOS_KEY);
+
+const savedToDos = localStorage.getItem(TODOS_KEY);
 console.log(savedToDos);
-if(savedToDos){
-  const parsedToDos=JSON.parse(savedToDos);
-  // console.log(parsedToDos);
-  parsedToDos.forEach(sayHello);//함수에 각 요소 전달함.
+if (savedToDos) {
+  const parsedToDos = JSON.parse(savedToDos);
+  // parsedToDos.forEach(sayHello);//함수에 각 요소 전달함.
   // parsedToDos.forEach((item)=>console.log("this is turn of a "+item));//람다식?
+  toDos = parsedToDos;
+  parsedToDos.forEach(paintToDo);
 }
