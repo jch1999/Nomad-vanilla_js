7ec4fc0d36ac1670e9787e913b08877962c9f3f2
jch1999 <54787512+jch1999@users.noreply.github.com>
2023-05-12 07:51:19 +0000

    weather api

--- MOMEMTUM/js/weather.js
+++ MOMEMTUM/js/weather.js
@@ -1,15 +1,21 @@
-const API_KEY="0b1e63e53c72076a47f2d457a453354e";
+const API_KEY = "0b1e63e53c72076a47f2d457a453354e";
 
-function onGeoOk(position){
-    const lat=position.coords.latitude;
-    const lon=position.coords.longitude;
-    console.log("You libe in, ",lat,lon);
-    const url=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`;
-    console.log(url);
+function onGeoOk(position) {
+  const lat = position.coords.latitude;
+  const lon = position.coords.longitude;
+  console.log("You libe in, ", lat, lon);
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+  // console.log(url);
+  fetch(url).then(response => response.json()).then(data => {
+    const weather = document.querySelector("#weather span:first-child");
+    const city = document.querySelector("#weather span:last-child");
+    city.innerText = data.name;
+    weather.innerText = `${data.weather[0].main}/${data.main.temp}`;
+  })
 }
 
-function onGeoError(){
-    alert("Can't find you. Mo weather for you.");
+function onGeoError() {
+  alert("Can't find you. Mo weather for you.");
 }
 
-navigator.geolocation.getCurrentPosition(onGeoOk,onGeoError);
+navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
